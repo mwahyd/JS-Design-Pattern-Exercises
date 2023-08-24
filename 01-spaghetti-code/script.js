28cdece849f3c785d3768c076353eafea6df2cbf
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-08-24 18:10:02 +0200

    add dummy names dynamically to DOM when document loaded

--- 01-spaghetti-code/script.js
+++ 01-spaghetti-code/script.js
@@ -2,6 +2,9 @@ const input = document.querySelector("input");
 const submit = document.querySelector("button");
 const ul = document.querySelector("#list");
 
+// vars
+const people = ["william", "laura", "simone", "davids"];
+
 // handlers
 function addItem(event) {
   event.preventDefault();
@@ -15,9 +18,32 @@ function addItem(event) {
   console.log(name);
 }
 
+function displayOnDOM(event) {
+  ul.innerHTML = "";
+  people.forEach((person, index) => {
+    const li = document.createElement("li");
+    const name = document.createElement("span");
+    const del = document.createElement("span");
+
+    li.setAttribute("data-index", index);
+    name.classList.add("name");
+    del.classList.add("delete");
+
+    name.textContent = person;
+    del.textContent = "X";
+
+    li.appendChild(name);
+    li.appendChild(del);
+
+    ul.appendChild(li);
+  });
+}
+
 // action functions
 function isInput() {
   input.value.trim() === "" ? false : input.value.trim().toLowerCase();
 }
+
 // event listeners
 submit.addEventListener("click", addItem);
+document.addEventListener("DOMContentLoaded", displayOnDOM);
