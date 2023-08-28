c06beaf5e38a4a8af0d89cb5329d7a546b84027d
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-08-28 11:35:12 +0200

    create the basic files

---
+++ 06-module-pattern/script.js
@@ -0,0 +1,8 @@
+/* 
+! The factory functions and the Module Pattern are very similar. 
+The main difference is how they are created.
+
+- The "Module Pattern" uses private and public attributes and methods // * BUT it is invoked IMMEDIATELY IIFE
+
+- Facotry Functions also uses private and public attrs and methods // * BUT can be used over and over again
+*/
